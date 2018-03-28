971b5e6973867cabbc52875d042aed30a71c63fc
Chris Filippis <chrisfilippis>
2018-03-28 20:38:05 +0300

    get unique combinations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from scipy.spatial import KDTree
+import itertools
 
 # The radius of points to return.
 radius = 50
@@ -12,46 +13,48 @@ eps = 1
 k = 5
 
 def find_nearest(restaurants_kd_tree, r, hotel):
-    idx = restaurants_kd_tree.query_ball_point(hotel, r=r, p=p_norm)
-    return len(idx)
+    idx = restaurants_kd_tree.query_ball_point([hotel[1], hotel[2]], r=r, p=p_norm)
+    return [hotel[0], len(idx)]
 
 def find_score_for_positions(positions, r, restaurants_tree):
-
-    print 'looping through positions..'
-    for position in positions:
-        print position
-        print find_nearest(restaurants_tree, r, position)
-
-def find_knn(tree, position, k_points):
-    return tree.query(position, k=k_points)
-
-def find_knn_score_for_positions(positions, k_points, restaurants_tree):
-    print 'looping through positions..'
     positions_data = list()
     for position in positions:
-        distances, indices = find_knn(restaurants_tree, position, k_points=k_points)
-        if(len(distances) > 0):
-            positions_data.append(distances[-1])
+        positions_data.append(find_nearest(restaurants_tree, r, position))
     return positions_data
 
-print 'loading data..'
+def find_knn(tree, hotel, k_points):
+    ind, distance = tree.query([hotel[1], hotel[2]], k=k_points)
+    return [hotel[0], distance[-1]]
 
-restaurants = pd.read_csv('C:\Users\cfilip09\Downloads\\vlahou\\restaurants-ver1.txt', sep="|", header=1, names = ['id','name','rating','latitude','longitude','cousine']).as_matrix()
+def find_knn_score_for_positions(hotels, k_points, restaurants_tree):
+    positions_data = list()
+    
+    for hotel in hotels:
+        knn = find_knn(restaurants_tree, hotel, k_points=k_points)
+        positions_data.append(knn)
+
+    return positions_data
+
+# loading data..
 hotels = pd.read_csv('C:\Users\cfilip09\Downloads\\vlahou\hotels-ver1.txt', sep="|", header=1, names = ['id','name','rating','rating1','latitude','longitude','cousine']).as_matrix()
+restaurants = pd.read_csv('C:\Users\cfilip09\Downloads\\vlahou\\restaurants-ver1.txt', sep="|", header=1, names = ['id','name','rating','latitude','longitude','cousine']).as_matrix()
 
-print 'indexing..'
+# indexing
 tree = KDTree(restaurants[:, [3, 4]])
 
-print 'task 1..'
-input_hotels = hotels[:, [4, 5]][:3]
-find_score_for_positions(input_hotels, radius, tree)
-print 'task 1 done..'
+np.random.shuffle(hotels)
+input_hotels = hotels[:, [0, 4, 5]][:3]
 
+# task 1..
+score_positions = find_score_for_positions(input_hotels, radius, tree)
+print sorted(score_positions, key=lambda x: x[1])
 
-print 'task 2..'
-print find_knn_score_for_positions(input_hotels, k, tree)
-print 'task 2 done..'
+# task 2..
+knn_score_positions = find_knn_score_for_positions(input_hotels, k, tree)
+print sorted(knn_score_positions, key=lambda x: x[1])
 
-# print sorted(hotels_score, key=lambda a_entry: a_entry[1], reverse=True)[:50]
-#for hotel in hotels:
-#    idx = tree.query([hotel[3], hotel[4]], k, p=)
+print '---------------------'
+print input_hotels[:, [1, 2]]
+dd = itertools.combinations(tuple([tuple(row) for row in input_hotels[:, [1, 2]]]), 2)
+print set(dd)
+
